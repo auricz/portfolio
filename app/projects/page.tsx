f14463b3a4829835e990bed4b5896feec09add1d
Auric Z <67848372+auricz@users.noreply.github.com>
2026-09-09 15:30:53 -0400

    Update page.tsx in projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,7 +3,7 @@ import SoftwareSection from "@/components/project/ProjectSection";
 import { getSiteData, SiteData } from "@/lib/data";
 
 export const metadata: Metadata = {
-  title: `Auric Z. — Projects`,
+  title: `Auric Z. | Projects`,
   description: "Featured software projects that I have made for school or for work."
 };
 
